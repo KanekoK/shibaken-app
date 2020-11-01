84185df20e7dd29e6f44b6108fd0cacfdbfb25ca
kaneko <b1r2d3q4@gmail.com>
2020-11-01 16:15:06 +0900

    .

---
+++ src/main/java/com/example/shibakenapp/ShibakenApiController.java
@@ -0,0 +1,33 @@
+package com.example.shibakenapp;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("api")
+public class ShibakenApiController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/")
+    public String show() {
+        return "Hello world";
+    }
+
+    @GetMapping("/shibaken")
+    public List<String> getShibaken() {
+
+        String url = "http://shibe.online/api/shibes";
+
+        String[] strings = restTemplate.getForObject(url, String[].class);
+
+        return Arrays.asList(strings);
+    }
+}
--- src/main/java/com/example/shibakenapp/ShibakenAppController.java
+++ src/main/java/com/example/shibakenapp/ShibakenAppController.java
@@ -1,31 +1,16 @@
 package com.example.shibakenapp;
 
-import java.util.Arrays;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping("page")
 public class ShibakenAppController {
 
-    @Autowired
-    private RestTemplate restTemplate;
-
     @GetMapping("/")
-    public String show() {
-        return "Hello world";
+    public String index() {
+        return "index";
     }
 
-    @GetMapping("/api/shibaken")
-    public List<String> getShibaken() {
-
-        String url = "http://shibe.online/api/shibes";
-
-        String[] strings = restTemplate.getForObject(url, String[].class);
-
-        return Arrays.asList(strings);
-    }
 }
